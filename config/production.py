2e4cd9d83cb8aff3effa0cacd560322b9021e502
Fabian Ehrler <fabian.ehrler@netrics.ch>
2023-05-04 19:08:10 +0200

    add missing requirement/module remove unused ref. to postgresql.

--- config/production.py
+++ config/production.py
@@ -8,8 +8,8 @@ CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE
 
 # Configure Postgres database based on connection string of the libpq Keyword/Value form
 # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
-conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+#conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+#conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 
 # Connection string for a postgres db
 # DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
