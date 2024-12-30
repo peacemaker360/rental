1e7d819deb82f816356f88dae78364235772ec63
Fabian Ehrler <ehrler.fabian@gmail.com>
2024-12-30 17:12:52 +0100

    refactor: update customer details template to include status and last updated fields

--- app/models/Customer.py
+++ app/models/Customer.py
@@ -1,6 +1,6 @@
 # app/models/Customer.py
 from datetime import datetime, timezone
-from sqlalchemy import or_
+from sqlalchemy import JSON, or_
 
 from app import db
 
@@ -21,6 +21,8 @@ class Customer(db.Model):
         db.DateTime(timezone=True),
         onupdate=lambda: datetime.now(timezone.utc)
     )
+    is_active = db.Column(db.Boolean, default=True)
+    groups = db.Column(db.String(200), nullable=True)
     rental = db.relationship('Rental', backref='customer', lazy=True)
 
     @property
@@ -30,12 +32,35 @@ class Customer(db.Model):
     def __repr__(self):
         return f'<Customer {self.firstname} {self.lastname}>'
 
-    def __init__(self, firstname=None, lastname=None, email=None, phone=None):
+    def __init__(self, firstname=None, lastname=None, email=None, phone=None, groups=None):
         self.firstname = firstname
         self.lastname = lastname
         # self.name = f"{firstname} {lastname}"
         self.email = email
         self.phone = phone
+        self.groups = groups
+        self.update_active_status()
+
+    def update_active_status(self):
+        # Check if self.groups contains a specific group ID (e.g., '3742')
+        if self.groups and '3742' in self.groups:
+            self.is_active = True
+        else:
+            self.is_active = False
+
+    def set_groups(self, groups):
+        self.groups = groups
+        self.update_active_status()
+
+    def deactivate(self):
+        self.is_active = False
+        db.session.commit()
+        return self
+
+    def activate(self):
+        self.is_active = True
+        db.session.commit()
+        return self
 
     @staticmethod
     def search_customers(keyword):
